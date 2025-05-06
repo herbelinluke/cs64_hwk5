c6904a23acbcac83f1580a48af8d5cdd182af8e3
Luke Herbelin <lukeherbelin@ucsb.edu>
2025-05-05 21:35:51 -0700

    start

---
+++ disassembly_skeleton.cpp
@@ -0,0 +1,41 @@
+#include <iostream>     // for use of cin, cout, endl
+#include <string>       // for use of strings
+#include <cmath>        // for use of pow(x,y) function
+// NO OTHER LIBRARIES ARE ALLOWED!
+
+using namespace std;
+
+// function: disassemble takes in a string representing
+//          an assembled MIPS instruction as a string
+//          and returns the instruction itself
+//
+//          Level 1 difficulty: assume the input is:
+//          a) legitimate (an actual MIPS instruction)
+//          b) is ONLY an I-type and ONLY one of: addi, addiu, andi, ori, slti
+//          c) has only registers $t0 thru $t7 or $s0 thru $s7 in the instruction
+//
+string disassemble( string hex ) {
+
+    // You want to retrieve all the separate fields of an I-type instruction
+    //      to help you figure out what the assembly instruction is.
+    //
+    // HINT: Use bitwise masking and bit-shifting to isolate the different parts of the hex instruction!
+    //
+    // YOUR CODE GOES HERE!!
+ 
+    return "stub";  // remove stub and replace it with correct variable
+}
+
+int main() {
+// Don't change any code in main() function!!
+
+    string hex, inst;
+    
+    cout << "Enter your assembled instruction as a hex number: 0x";
+    cin >> hex;
+    
+    inst = disassemble( hex );
+    cout << "Your instruction is:\n" << inst << endl;
+
+    return 0;
+}
